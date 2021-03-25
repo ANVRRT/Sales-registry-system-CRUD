60bbbb12cb205ba9f640229bc93b41825bfb2a42
A01367213 <destroyer_all@hotmail.es>
2021-03-25 08:50:28 -0600

    Added compania form

---
+++ php/FC_capturar_Compania.php
@@ -0,0 +1,24 @@
+
+
+
+<div style="height:100%;">
+	<div >
+		<h1>Capturar Compañia</h1>
+		<form class="formulario">
+			<div class="campo">
+				<label class="campo__label" for="cliente">Id de Compañia</label>
+				<input class="campo__field" type="text" id="cliente">
+			</div>
+
+			<div class="campo">
+				<label class="campo__label" for="entrega">Nombre Compañia</label>
+				<input class="campo__field" type="text" id="entrega">
+			</div>
+
+			<div class="campo campo--button">
+				<input class="campo__field button--red grd" type="reset" value="Limpiar">
+				<input class="campo__field button--blue" type="submit" value="Guardar">
+			</div>
+		</form>
+	</div>	
+</div>
