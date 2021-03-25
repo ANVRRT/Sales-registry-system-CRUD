85b752927228c8bdcea9c17e5ea17d81c3b4cde1
A01422954 <A01422954@itesm.mx>
2021-03-25 08:12:03 -0600

    Add to NavBard Catálogos

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -73,16 +73,20 @@
     <!-- Nav Item - Utilities Collapse Menu -->
     <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Utilities</span>
+            <i class="fas fa-book"></i>
+            <span>Catálogos</span>
         </a>
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Utilities:</h6>
-                <a class="collapse-item" href="utilities-color.html">Colors</a>
-                <a class="collapse-item" href="utilities-border.html">Borders</a>
-                <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                <a class="collapse-item" href="utilities-other.html">Other</a>
+                <h6 class="collapse-header">Altas y bajas:</h6>
+                <a class="collapse-item" href="#">Compañia</a>
+                <a class="collapse-item" href="#">Inventario</a>
+                <a class="collapse-item" href="#">Almacen</a>
+                <a class="collapse-item" href="#">Cliente</a>
+                <a class="collapse-item" href="#">Agente</a>
+                <a class="collapse-item" href="#">Artículo</a>
+                <a class="collapse-item" href="#">Dir. Entrega</a>
+                <a class="collapse-item" href="#">Lista Precios</a>
             </div>
         </div>
     </li>
