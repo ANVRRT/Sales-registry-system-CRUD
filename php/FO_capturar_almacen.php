03b9918ff50ad8bea7376c6a54d0f925eb95b1bf
Javier Jimenez Barbosa <javier@MacBook-Pro-de-Javier.local>
2021-03-25 08:40:54 -0600

    Agregar Almacen

---
+++ php/FO_capturar_almacen.php
@@ -0,0 +1,32 @@
+<div style="height:100%;">
+	<div >
+		<h1>Capturar Almacen</h1>
+		<form class="formulario">
+			<div class="campo">
+				<label class="campo__label" for="id">ID Almacen</label>
+				<input class="campo__field" type="text" id="id">
+			</div>
+
+			<div class="campo">
+				<label class="campo__label" for="idC">ID Compañia</label>
+				<input class="campo__field" type="text" id="idC">
+			</div>
+
+			<div class="campo">
+				<label class="campo__label" for="compra">Orden de Compra</label>
+				<input class="campo__field" type="text" id="compra">
+			</div>
+
+			<div class="campo__text">
+				<label class="campo__label" for="desc">Descripción</label>
+				<textarea class="campo__field campo__field--textarea" id="desc"></textarea>
+			</div>
+
+
+			<div class="campo campo--button">
+				<input class="campo__field button--red grd" type="reset" value="Limpiar">
+				<input class="campo__field button--blue" type="submit" value="Guardar">
+			</div>
+		</form>
+	</div>	
+</div>
