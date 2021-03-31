2734f3e2073f085c1781391fbc0154b77251c5fb
Grecia Pacheco <A01366730@itesm.mx>
2021-03-30 23:17:07 -0600

    Modified sidebar

---
+++ includes/sidebar.php
@@ -0,0 +1,178 @@
+<?php
+session_start();
+if (!isset($_SESSION["idUsuario"])) {
+    header("location: ../php/login.php");
+    exit();
+}
+?>
+<ul style="height: 100%; vertical-align:middle;" class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar" ">
+
+    <!-- Sidebar - Brand -->
+    <a class=" sidebar-brand d-flex align-items-center justify-content-center" href="./index.php">
+    <div class="sidebar-brand-icon rotate-n-15">
+        <i class="fas fa-box-open"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">Cartones Corrugados</div>
+
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="./index.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Inicio</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Catálogos
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <!-- <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Components</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Custom Components:</h6>
+                <a class="collapse-item" href="buttons.html">Buttons</a>
+                <a class="collapse-item" href="cards.html">Cards</a>
+            </div>
+        </div>
+    </li> -->
+
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseA" aria-expanded="true" aria-controls="collapseA">
+            <i class="fas fa-check-double"></i>
+            <span>Autorizaciones</span>
+        </a>
+        <div id="collapseA" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Autorizaciones:</h6>
+                <a class="collapse-item" href="A_ordenes_venta.php">Ordenes de Venta</a>
+                <a class="collapse-item" href="A_fechas_definitivas.php">Ordenes de Venta</a>
+            </div>
+        </div>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseO" aria-expanded="true" aria-controls="collapseO">
+            <i class="fas fa-clipboard-list"></i>
+            <span>Ordenes</span>
+        </a>
+        <div id="collapseO" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Ordenes:</h6>
+                <a class="collapse-item" href="O_venta_proceso.php">Ordenes en proceso</a>
+                <a class="collapse-item" href="O_venta_procesada.php">Ordenes procesadas</a>
+                <a class="collapse-item" href="O_capturar.php">Capturar Orden</a>
+            </div>
+        </div>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#admin" aria-expanded="true" aria-controls="collapseUtilities">
+            <i class="fas fa-book"></i>
+            <span>Administracion</span>
+        </a>
+        <div id="admin" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Altas y bajas:</h6>
+                <a class="collapse-item" href="ADM_permisos.php">Permisos</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Utilities Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+            <i class="fas fa-book"></i>
+            <span>Catálogos</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Altas y bajas:</h6>
+                <a class="collapse-item" href="C_compania.php">Compañia</a>
+                <a class="collapse-item" href="C_inventario.php">Inventario</a>
+                <a class="collapse-item" href="C_almacen.php">Almacen</a>
+                <a class="collapse-item" href="C_cliente.php">Cliente</a>
+                <a class="collapse-item" href="C_agente.php">Agente</a>
+                <a class="collapse-item" href="C_articulo">Artículo</a>
+                <a class="collapse-item" href="C_dirEnt">Dir. Entrega</a>
+                <a class="collapse-item" href="C_listaPrecios">Lista Precios</a>
+                <a class="collapse-item" href="C_factura.php">Facturas</a>
+                <a class="collapse-item" href="C_cantidadE.php">Cant. Entregada</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Addons
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Pages</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Login Screens:</h6>
+                <a class="collapse-item" href="../php/login.php">Login</a>
+                <a class="collapse-item" href="../php/register.php">Register</a>
+                <?php
+                if (isset($_SESSION["idUsuario"])) {
+                    echo "<a class='collapse-item' href='../includes/logout.inc.php'>Desloggeate bro</a>";
+                } else {
+                    echo "<a class='collapse-item' href='../php/Login.php' >Loggeate bro</a>";
+                }
+
+                ?>
+                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+                <div class="collapse-divider"></div>
+                <h6 class="collapse-header">Other Pages:</h6>
+                <a class="collapse-item" href="404.html">404 Page</a>
+                <a class="collapse-item" href="blank.html">Blank Page</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Charts -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>Charts</span></a>
+    </li>
+
+    <!-- Nav Item - Tables -->
+    <li class="nav-item">
+        <a class="nav-link" href="O_venta.php">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Tables</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+
+
+</ul>
