8f616b0b1931ac3f61a685a01280935a01ac4b17
Angel009 <angelus007.ans@gmail.com>
2021-04-03 02:25:45 -0600

    75% of backend works (consult and unlock done)

--- includes/functions_catalogos.php
+++ includes/functions_catalogos.php
@@ -24,12 +24,14 @@ if(isset($_POST["B_artE"])){
 // if(isset($_POST["B_artE"])){ 
 //     deleteArtExistente($conn,$_POST["idArticulo"]);
 // }
-// if(isset($_POST["A_artE"])){ //BloqueoCliente
-//     createArtExistente($conn,$_POST["idArticulo"],$_POST["idCompania"],$_POST["descripcion"],$_POST["costo"]);
-// }
-// if(isset($_POST["B_artE"])){
-//     deleteArtExistente($conn,$_POST["idArticulo"]);
-// }
+//if(isset($_REQUEST['estado'])==0){//BLOQUEO CLIENTE
+  //  bClient($conn, $_GET['idB']);
+    //echo($_REQUEST['idB']);
+//}
+if(isset($_REQUEST['estado'])==1){
+    dClient($conn, $_REQUEST['idD']);
+    //echo($_REQUEST['idD']);
+}
 // if(isset($_POST["A_artE"])){ //CantEnt
 //     createArtExistente($conn,$_POST["idArticulo"],$_POST["idCompania"],$_POST["descripcion"],$_POST["costo"]);
 // }
@@ -151,6 +153,72 @@ function deleteArtExistente($conn,$idArticulo){
     else{
         mysqli_stmt_close($stmt);
         header("location: ../php/C_articuloExistente.php?error=sqlerror");
+        exit();
+    }
+}
+
+function disClients($conn, $estado){
+    $sql="SELECT * FROM Cliente WHERE bloqueo = ?";
+
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt,$sql))
+    {
+        header("location: ../php/index.php?error=stmtfailed");
+        exit();
+    }
+    mysqli_stmt_bind_param($stmt,"i", $estado);
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+    return $resultData;
+
+    mysqli_stmt_close($stmt);
+}
+
+function bClient($conn, $id){
+    $sql= "UPDATE Cliente SET bloqueo= 0 WHERE idCliente= ?";
+
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt,$sql))
+    {
+        header("location: ../php/index.php?error=stmtfailed");
+        exit();
+    }
+
+    mysqli_stmt_bind_param($stmt,"i",$id);
+    if(mysqli_stmt_execute($stmt))
+    {
+        mysqli_stmt_close($stmt);
+        //header("location: ../php/C_bloqueoCliente.php?error=success");
+        exit();
+    }
+    else{
+        mysqli_stmt_close($stmt);
+        //header("location: ../php/C_bloqueoCliente.php?error=sqlerror");
+        exit();
+    }
+}
+
+function dClient($conn, $id){
+    $sql= "UPDATE Cliente SET bloqueo= 1 WHERE idCliente= ?";
+
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt,$sql))
+    {
+        header("location: ../php/index.php?error=stmtfailed");
+        exit();
+    }
+
+    mysqli_stmt_bind_param($stmt,"i",$id);
+    if(mysqli_stmt_execute($stmt))
+    {
+        mysqli_stmt_close($stmt);
+        header("location: ../php/C_bloqueoCliente.php?error=success");
+        exit();
+    }
+    else{
+        mysqli_stmt_close($stmt);
+        header("location: ../php/C_bloqueoCliente.php?error=sqlerror");
         exit();
     }
 }
--- php/forms/FC_bloqueoCliente.php
+++ php/forms/FC_bloqueoCliente.php
@@ -1,3 +1,7 @@
+<?php
+    require_once("../includes/dbh.inc.php");
+    require_once("../includes/functions_catalogos.php");
+?>
 <!DOCTYPE html>
 <html lang="es">
     <head>
@@ -5,49 +9,68 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Bloqueo de clientes</title>
-
     </head>
     <body>
         <div class="fix-margin">
-            <form action="#" method="POST" class="formulario">
-                <br>
-                <h2>Bloqueo de clientes</h2>
-                <br>
-                <table style="width:100%" class="table">
-                    <thead class="thead-light">
-                        <tr>
-                            <th><label class="campo__label" >Disponibles</label></th>
-                            <th><label class="campo__label" >Bloqueados</label></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
+        <br>
+            <h2>Bloqueo de clientes</h2>
+            <br>
+            <table style="width:100%" class="table">
+                <thead class="thead-light">
+                    <tr>
+                        <th><label class="campo__label" >Disponibles</label></th>
+                        <th><label class="campo__label" >Bloqueados</label></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
                             <table style="width: 100%;">
-                            
-                                <tr>
-                                    <td>Alfreds Futterkiste</td>
-                                    <td>
-                                        <input class="btn btn-danger" type="button" value="Bloquear">
-                                    </td>
-                                </tr>
+                            <?php
+                                $clientesD = disClients($conn, 1);
+                                $check=mysqli_fetch_assoc($clientesD);
+                                if($check){
+                                    while($row = mysqli_fetch_assoc($clientesD))
+                                    {
+                                        echo"<tr>";
+                                            echo"<td>".$row["nombreCliente"]."</td>";
+                                            echo"<td>";
+                                                echo"<a href='../includes/functions_catalogos.php?estado=0&idB=".$row["idCliente"]."'class='btn btn-danger'>Bloquear</a></th>";
+                                            echo"</td>";
+                                        echo"</tr>";
+                                    }
+                                }
+                                else{
+                                    echo"No hay clientes disponibles";
+                                }
+                            ?>
                             </table>
                             </td>
-                            
                             <td>
                             <table style="width: 100%;">
-                                <tr>
-                                    <td>Alfreds Futterkiste</td>
-                                    <td>
-                                        <input class="btn btn-success" type="button" value="Desbloquear">
-                                    </td>
-                                </tr>
+                            <?php
+                                $clientesB = disClients($conn, 0);
+                                $check=mysqli_fetch_assoc($clientesB);
+                                if($check){
+                                    while($row = mysqli_fetch_assoc($clientesB))
+                                    {
+                                        echo"<tr>";
+                                            echo"<td>".$row["nombreCliente"]."</td>";
+                                            echo"<td>";
+                                                echo"<a href='../includes/functions_catalogos.php?estado=1&idD=".$row["idCliente"]."'class='btn btn-success'>Desbloquear</a></th>";
+                                            echo"</td>";
+                                        echo"</tr>";
+                                    }
+                                }
+                                else{
+                                    echo"No hay clientes bloqueados";
+                                }
+                            ?>
                             </table>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </form>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </body>
 </html>
