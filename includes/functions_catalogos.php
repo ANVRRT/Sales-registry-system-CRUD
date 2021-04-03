c4599ea55abab6dcbf5ddd35db07a47a182b0048
Tupm3 <jediulises@yahoo.com.mx>
2021-04-02 22:37:58 -0600

    Added facturas functions

--- includes/functions_catalogos.php
+++ includes/functions_catalogos.php
@@ -54,12 +54,6 @@ if(isset($_POST["B_artE"])){
 // if(isset($_POST["B_artE"])){
 //     deleteArtExistente($conn,$_POST["idArticulo"]);
 // }
-// if(isset($_POST["A_artE"])){ //Factura
-//     createArtExistente($conn,$_POST["idArticulo"],$_POST["idCompania"],$_POST["descripcion"],$_POST["costo"]);
-// }
-// if(isset($_POST["B_artE"])){
-//     deleteArtExistente($conn,$_POST["idArticulo"]);
-// }
 // if(isset($_POST["A_artE"])){ //Inventario
 //     createArtExistente($conn,$_POST["idArticulo"],$_POST["idCompania"],$_POST["descripcion"],$_POST["costo"]);
 // }
@@ -84,6 +78,13 @@ if(isset($_POST["B_artE"])){
 //     deleteArtExistente($conn,$_POST["idArticulo"]);
 // }
 
+//FACTURAS
+if(isset($_POST["B_Facs_Alta"])){ //Factura
+    createFactura($conn,$_POST["numFac"],$_POST["idCompania"],$_POST["idOrden"],$_POST["idArticulo"],$_POST["idCliente"],$_POST["idFolio"],$_POST["entrega"],$_POST["tipoTrans"],$_POST["fechaFac"]);
+}
+if(isset($_POST["B_Facs_Baja"])){
+    deleteFactura($conn,$_POST["numFac"]);
+}
 
 function dispArticulos($conn, $idCompania){
     $sql="SELECT * FROM ArticuloExistente WHERE idCompania = ?";
@@ -154,4 +155,106 @@ function deleteArtExistente($conn,$idArticulo){
         exit();
     }
 }
+
+
+function dispClientes($conn)
+{
+    $sql="SELECT * FROM Cliente";
+
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt,$sql))
+    {
+        header("location: ../php/index.php?error=stmtfailed");
+        exit();
+    }
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+    if(mysqli_fetch_assoc($resultData))
+    {
+        return $resultData;
+    }
+    else{
+        $result = "No hay clientes existentes";
+        return $result;
+    }
+
+    mysqli_stmt_close($stmt);
+}
+
+function getIDCompany($conn, $idCliente)
+{
+    $sql = "SELECT idCompania FROM Cliente WHERE idCliente = ?";
+
+    $statement = mysqli_stmt_init($conn);
+    
+    if(!mysqli_stmt_prepare($statement, $sql))
+    {
+        header("location: ../php/index.php?error=stmtfailed");
+        exit();
+    }
+    mysqli_stmt_bind_param($statement,"s",$idCliente);
+    mysqli_stmt_execute($statement);
+
+    $resultData = mysqli_stmt_get_result($statement);
+    if(mysqli_fetch_assoc($resultData))
+    {
+        return $resultData;
+    }
+    else{
+        $result = "No hay compañia registrada...";
+        return $result;
+    }
+
+    mysqli_stmt_close($statement);
+
+}
+
+function createFactura($conn,$numFact,$idCompania,$idOrden,$idArticulo,$idCliente,$folio,$entrega,$tipoTrans,$fechaFac)
+{
+    $sql = "INSERT INTO Factura VALUES(?,?,?,?,?,?,?,?,?);";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt,$sql))
+    {
+        header("location: ../php/index.php?error=stmtfailed");
+        exit();
+    }
+
+    mysqli_stmt_bind_param($stmt,"sssssiiss",$numFact,$idCompania,$idOrden,$idArticulo,$idCliente,$folio,$entrega,$tipoTrans,$fechaFac);
+    if(mysqli_stmt_execute($stmt))
+    {
+        mysqli_stmt_close($stmt);
+        header("location: ../php/C_factura.php?error=success");
+        exit();
+    }
+    else{
+        //echo mysqli_error($conn);
+        mysqli_stmt_close($stmt);
+        header("location: ../php/C_factura.php?error=sqlerror");
+        exit();
+    }
+}
+
+function deleteFactura($conn,$numFact){
+    $sql = "DELETE FROM Factura WHERE numFact = ?";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt,$sql))
+    {
+        header("location: ../php/index.php?error=stmtfailed");
+        exit();
+    }
+    mysqli_stmt_bind_param($stmt,"s",$numFact);
+    if(mysqli_stmt_execute($stmt))
+    {
+        mysqli_stmt_close($stmt);
+        header("location: ../php/C_factura.php?error=success");
+        exit();
+    }
+    else{
+        mysqli_stmt_close($stmt);
+        header("location: ../php/C_factura.php?error=sqlerror");
+        exit();
+    }
+}
+
 ?>
--- php/forms/FC_factura.php
+++ php/forms/FC_factura.php
@@ -1,71 +1,96 @@
 
+
 <div class="formulario">
 	<h1 class="h1-mine">Facturas</h1>
 
-	<form class="formulario">
-		<div class="campo">
-			<label class="campo__label" for="idCompania">Compañia</label>
-			<input class="campo__field" type="text" id="idCompania">
-		</div>
+	<form class="formulario" method="POST" action="../includes/functions_catalogos.php">
 
 		<div class="campo">
 			<label class="campo__label" for="idCliente">ID Cliente</label>
-			<input class="campo__field" type="text" id="idCliente">
+			<input class="campo__field" type="text" name="idCliente" id="idCliente" list="clientes" required>
+			<?php
+				require_once("../includes/dbh.inc.php");
+				require_once("../includes/functions_catalogos.php");
+				$reg = dispClientes($conn);
+				
+				echo "<datalist id='clientes'>";
+				while($row = mysqli_fetch_assoc($reg))
+				{
+					echo "<option>".$row["idCliente"]."</option>";
+					$idCompany = $row["idCompania"];
+
+				}
+				
+				echo "</datalist>";
+			?>
+		</div>
+
+		<div class="campo">
+			<label class="campo__label" for="idCompania">Compañia</label>
+			<?php
+				$idCliente = "<script>document.writeln(document.getElementById('idCliente').innerHTML);</script>";
+				if($idCliente != "")
+				{
+					//$idCompany = getIDCompany($conn, $idCliente);
+					echo "<input class='campo__field' type='text'name='idCompania' id='idCompania' value='$idCompany' readonly>";	
+				}
+			?>
 		</div>
 
 		<div class="campo">
 			<label class="campo__label" for="idOrden">ID Orden</label>
-			<input class="campo__field" type="text" id="idOrden">
+			<input class="campo__field" type="text" name="idOrden" maxlength="10" required>
 		</div>
 
 		<div class="campo">
-			<label class="campo__label" for="idPosicion">ID Posicion</label>
-			<input class="campo__field" type="text" id="idPosicion">
+			<label class="campo__label" for="idPosicion">Folio</label>
+			<input class="campo__field" type="text" name="idFolio" maxlength="11" required>
 		</div>
 
 		<div class="campo">
 			<label class="campo__label" for="idEntrega">Entrega</label>
-			<input class="campo__field" type="text" id="idEntrega">
+			<input class="campo__field" type="text" name="entrega" maxlength="11" required>
 		</div>
 
 		<div class="campo">
 			<label class="campo__label" for="tipoTrans">Tipo de Transaccion</label>
-			<input class="campo__field" type="text" id="tipoTrans">
+			<input class="campo__field" type="text" name="tipoTrans" maxlength="4" required>
 		</div>
 
 		<div class="campo">
 			<label class="campo__label" for="numFac">Número de Factura</label>
-			<input class="campo__field" type="text" id="numFac">
+			<input class="campo__field" type="text" name="numFac" maxlength="10" required>
 		</div>
 
 		<div class="campo">
 			<label class="campo__label" for="fechaFac">Fecha de Facturacion</label>
-			<input class="campo__field" type="date" id="fechaFac">
+			<input class="campo__field" type="date" name="fechaFac" required>
 		</div>
 
 		<div class="campo">
-			<div class="campo__icon">
-				<label class="campo__label" for="articulo">Artículo</label>
-				<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus campo__icon--icon" width="22" height="22" viewBox="0 0 24 24" stroke-width="2.5" stroke="#36b9cc" fill="none" stroke-linecap="round" stroke-linejoin="round">
-					<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-					<circle cx="12" cy="12" r="9" />
-					<line x1="9" y1="12" x2="15" y2="12" />
-					<line x1="12" y1="9" x2="12" y2="15" />
-				</svg>
-			</div>
-			<input class="campo__field" type="text" id="articulo">
-		</div>
+			<label class="campo__label" for="articulo">Artículo</label>
+			<input class="campo__field" type="text" name="idArticulo" id="idArticulo" list="articulos" required>
+			<?php
+				require_once("../includes/dbh.inc.php");
+				require_once("../includes/functions_catalogos.php");
+				$reg = dispArticulos($conn, $idCompany);
+				
+				echo "<datalist id='articulos'>";
+				while($row = mysqli_fetch_assoc($reg))
+				{
+					echo "<option>".$row["idArticulo"]."</option>";
 
-		<div class="campo campo__text">
-			<label class="campo__label" for="observacion-fac">Observaciones Facturacion</label>
-			<textarea class="campo__field campo__field--textarea" id="observacion-fac"></textarea>
+				}
+				
+				echo "</datalist>";
+			?>
 		</div>
 
 
 		<div class="campo__3--button">
 			<input class="campo__field button--red" style="grid-row: 3 / 4;" type="reset" value="Limpiar">
-			<input class="campo__field button--blue" type="submit" value="Baja">
-			<input class="campo__field button--blue" type="submit" value="Alta">
+			<input class="campo__field button--blue" type="submit" value="Baja" name ="B_Facs_Baja">
+			<input class="campo__field button--blue" type="submit" value="Alta" name ="B_Facs_Alta">
 		</div>
 	</form>
 </div>
