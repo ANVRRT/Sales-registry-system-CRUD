2ad6615cffd7798bc68e7b945d01daeb2b75d8aa
HOLA <paultrizixtyrl@gmail.com>
2021-04-04 15:26:23 -0500

    Update FC_agente.php

--- php/forms/FC_agente.php
+++ php/forms/FC_agente.php
@@ -2,9 +2,24 @@
 	<h1 class="h1-mine">Capturar Agente</h1>
 
 	<form class="formulario" method="POST" action="../includes/functions_catalogos.php">
-		<div class="campo">
+	<div class="campo">
 			<label class="campo__label" for="idRepresentante">ID Representante</label>
-			<input class="campo__field" type="text" name="idRepresentante" id="idRepresentante">
+			<input class="campo__field" type="text" name="idRepresentante" id="idRepresentante" list="representante" required>
+			<?php
+				require_once("../includes/dbh.inc.php");
+
+				require_once("../includes/functions_catalogos.php");
+				$reg = dispRepresentante($conn, $_SESSION["idCompania"]);
+				
+				echo "<datalist id='representante'>";
+				while($row = mysqli_fetch_assoc($reg))
+				{
+					echo "<option>".$row["idRepresentante"]."</option>";
+
+				}
+				
+				echo "</datalist>";
+			?>
 		</div>
 
 		<div class="campo">
