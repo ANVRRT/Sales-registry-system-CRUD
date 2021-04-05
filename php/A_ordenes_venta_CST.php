c252ead2a20075f0356aebdd0316b3c2020f9ece
A01422954 <A01422954@itesm.mx>
2021-04-05 00:45:17 -0500

    Modified sidebar

--- php/A_ordenes_venta_CST.php
+++ php/A_ordenes_venta_CST.php
@@ -38,7 +38,7 @@
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">Autorizar Ordenes de Venta CST</h1>
+                    <h1 class="h3 mb-2 text-gray-800">Autorización Ordenes de Venta CST</h1>
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
