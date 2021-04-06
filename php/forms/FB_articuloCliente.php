ad070f3796cbe043daa2a637fd73860c64e416e1
HOLA <paultrizixtyrl@gmail.com>
2021-04-06 13:20:36 -0500

    Update FB_articuloCliente.php

--- php/forms/FB_articuloCliente.php
+++ php/forms/FB_articuloCliente.php
@@ -6,12 +6,27 @@
 
         <div class="campo">
             <label class="campo__label" for="idCliente">Cliente</label>
-            <input class="campo__field" type="text" id="idCliente" name="idCliente">
+            <input class="campo__field" type="text"  name="idCliente" id="idCliente" list="cliente" required>
+            <?php
+                require_once("../includes/dbh.inc.php");
+
+                require_once("../includes/functions_catalogos.php");
+                $reg = dispClientes($conn, $_SESSION["idCompania"]);
+                
+                echo "<datalist id='cliente'>";
+                while($row = mysqli_fetch_assoc($reg))
+                {
+                    echo "<option>".$row["idCliente"]."</option>";
+
+                }
+                
+                echo "</datalist>";
+            ?>
         </div>
 
         <div class="campo__3--button">
             <input class="campo__field button--red" style="grid-row: 3 / 4;" type="reset" value="Limpiar">
-            <input class="campo__field button--blue" type="submit" value="Buscar" name="Buscar">
+            <input class="campo__field button--blue" type="submit" value="Buscar" name="Buscar_articuloCliente">
         </div>
     </form>
 </div>
