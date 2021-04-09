9ef2fb658292dfbd7a97cf2553a6263088159089
HOLA <paultrizixtyrl@gmail.com>
2021-04-09 00:04:56 -0500

    Modified datalists from forms Agente and Almacen

--- php/forms/FC_agente.php
+++ php/forms/FC_agente.php
@@ -11,7 +11,9 @@
 				echo "<datalist id='representante'>";
 				while($row = mysqli_fetch_assoc($reg))
 				{
-					echo "<option>".$row["idRepresentante"]."</option>";
+					if($row["estatus"]==1){
+						echo "<option>".$row["idRepresentante"]."</option>";
+					}
 
 				}
 				
