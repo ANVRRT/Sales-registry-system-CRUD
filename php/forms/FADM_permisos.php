e315e69fd7c89307e38e2261459afb1802694f6d
OpaqueBlock <sushitofox@gmail.com>
2021-04-14 20:34:21 -0500

    FADM cambio a permisos options y quitar logout modal de todos los archivos dentro del folder de PHP.

--- php/forms/FADM_permisos.php
+++ php/forms/FADM_permisos.php
@@ -11,18 +11,22 @@
 		<div class="campo">
 			<label class="campo__label" for="permiso">Permiso</label>
             <select name="permiso" class="campo__field">
-						<option>ADM Administrador</option>		
-						<option>AGE Agente</option>
-						<option>CST Costos</option>	
-						<option>CXC Cuentas por Cobrar</option>	
-						<option>DIR Dirección</option>	
-                        <option>EMB Embarques</option>
-                        <option>FAC Facturación</option>
-                        <option>FEC Fechas</option>
-                        <option>ING Ingeniería</option>
-                        <option>PLN Planeación</option>
-                        <option>REA Reaprovisionamientos</option>
-                        <option>VTA/PRE Ventas y Precios</option>
+						<option>p_permisos</option>		
+						<option>pb_artCliente</option>
+						<option>po_ordenesVenta</option>	
+						<option>pc_agente</option>	
+						<option>pc_almacen</option>	
+                        <option>pc_artExistente</option>
+                        <option>pc_artVendido</option>
+                        <option>pc_bloq_Cliente</option>
+                        <option>pc_cantEntregada</option>
+                        <option>pa_clientes</option>
+                        <option>pc_compania</option>
+                        <option>pc_dirEnt</option>
+						<option>pc_factura</option>
+                        <option>pc_inventario</option>
+                        <option>pc_listPrecios</option>
+                        <option>po_capturar</option>
 					</select>
 		</div>
 
