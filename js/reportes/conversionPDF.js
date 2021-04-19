596e08692c68a6fc4c78238bf01d8ff4931c137b
Tupm3 <jediulises@yahoo.com.mx>
2021-04-18 23:00:50 -0500

    Added Reporte Todas las Ordenes

--- js/reportes/conversionPDF.js
+++ js/reportes/conversionPDF.js
@@ -45,3 +45,26 @@ function generatePDF_TD() {
         }
     }).from(element).save();
 }
+
+function generatePDF_AO() {
+    const element = document.getElementById("tableDIV");
+
+    html2pdf()
+    .set({
+        margin: 1,
+        filename: 'ReporteTodasOrdenes.pdf',
+        image: {
+            type: 'jpeg',
+            quality: 0.98
+        },
+        html2canvas: {
+            scale: 2, // A mayor escala, mejores gráficos, pero más peso
+            letterRendering: true,
+        },
+        jsPDF: {
+            unit: "in",
+            format: "a2",
+            orientation: 'landscape' // landscape o portrait
+        }
+    }).from(element).save();
+}
