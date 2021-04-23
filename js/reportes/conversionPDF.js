80e7b370044a1e91f1fc8ec125687f5189191ddb
Angel009 <angelus007.ans.com>
2021-04-23 01:18:02 -0500

    minor changes

--- js/reportes/conversionPDF.js
+++ js/reportes/conversionPDF.js
@@ -1,10 +1,35 @@
-function generatePDF() {
+function generatePDF(name) {
     const element = document.getElementById("canvas-holder");
     
     html2pdf()
     .set({
         margin: 1,
-        filename: 'reporte.pdf',
+        filename: name,
+        image: {
+            type: 'jpeg',
+            quality: 0.98
+        },
+        html2canvas: {
+            scale: 5, // A mayor escala, mejores gráficos, pero más peso
+            letterRendering: true,
+        },
+        jsPDF: {
+            unit: "in",
+            format: "a3",
+            orientation: 'landscape' // landscape o portrait
+        }
+    })
+      .from(element)
+      .save();
+}
+
+function generatePDFR(name) {
+    const element = document.getElementById("canvas-holderR");
+    
+    html2pdf()
+    .set({
+        margin: 1,
+        filename: name,
         image: {
             type: 'jpeg',
             quality: 0.98
