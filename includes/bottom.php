b3b925146d745714490fea52269a50ea4e419904
A01422954 <A01422954@itesm.mx>
2021-04-25 21:54:52 -0500

    Modified bottom.php

--- includes/bottom.php
+++ includes/bottom.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
 <div class="container my-auto">
     <div class="copyright text-center my-auto">
-        <span>Copyright &copy; Cartones Corrugados 2020</span>
+        <span>Copyright &copy; Papeles Corrugados 2020</span>
     </div>
 </div>
 </footer>
